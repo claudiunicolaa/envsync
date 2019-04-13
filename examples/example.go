89e64a1e7b474c718a907c70f1266c497aa89610
Claudiu Nicola <claudiunicola96@gmail.com>
2019-04-13 23:07:30 +0300

    examples

--- examples/example.go
+++ examples/example.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/claudiunicolaa/envsync"
+)
 
+func main() {
+	// because environment files aren't into the root directory we need to add "examples/"
+	_, err := envsync.EnvSync("examples/.env", "examples/.env.example")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// all good, we'll supposed here is
+	// a lot of magic written in go
 }
